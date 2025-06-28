790e496dcd9e04f3009e0a90a0493d4138863159
Cremacious <chrismackall3@gmail.com>
2025-06-28 11:36:48 -0400

    Successful new user creation

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,27 @@
 import Link from 'next/link';
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+
 
 export default function Home() {
   return (
     <>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Link href={'/game'}>Game</Link>
     </>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 import '@/assets/styles/globals.css';
 import { type Metadata } from 'next';
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
 import { Geist, Geist_Mono } from 'next/font/google';
-
+import ConvexClerkProvider from '@/components/providers/ConvexClerkProvider';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -31,31 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        cssLayerName: 'clerk',
-      }}
-    >
-      <html lang="en">
+    <ConvexClerkProvider>
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    </ConvexClerkProvider>
   );
 }
